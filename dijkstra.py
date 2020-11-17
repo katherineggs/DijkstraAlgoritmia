9b2350dab53e6de5b89daaa6ab4bd22ebb7ca9eb
katherineggs <katherinegarcia@ufm.edu>
2020-11-17 12:11:23 -0600

    graph class

--- dijkstra.py
+++ dijkstra.py
@@ -42,3 +42,41 @@ class Node:
 
     def __str__(self):
         return str(self.node) + ' next: ' + str([x.id for x in self.next])
+
+class Graph:
+    def __init__(self):
+        self.allNodes = {}
+        self.cantNodos = 0
+        
+    def __iter__(self):
+        return iter(self.allNodes.values())
+
+    def addNode(self, node):
+        self.cantNodos = self.cantNodos + 1
+        newNode = Node(node)
+        self.allNodes[node] = newNode
+        return newNode
+
+    def getNode(self, num):
+        if num in self.allNodes:
+            return self.allNodes[num]
+        else:
+            return None
+
+    def addEdge(self, origen, destino, peso = 0):
+        if origen not in self.allNodes:
+            self.addNode(origen)
+        if destino not in self.allNodes:
+            self.addNode(destino)
+
+        self.allNodes[destino].addNext(self.allNodes[destino], peso)
+        self.allNodes[destino].addNext(self.allNodes[destino], peso)
+
+    def getNodes(self):
+        return self.allNodes.keys()
+
+    def setAnterior(self, actual):
+        self.anterior = actual
+
+    def getAnterior(self, actual):
+        return self.anterior

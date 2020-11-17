c89a583f05c30b0fea2510308347f0bce9555fe0
katherineggs <katherinegarcia@ufm.edu>
2020-11-16 19:07:56 -0600

    Try 1

--- dijkstra.py
+++ dijkstra.py
@@ -1,14 +1,21 @@
-
 def dijkstra(info, origin, destiny):
+    path=[]
+    camino = 0
     for nodo,arista in info.items():
         # verificar si existe un camino directo entre origen y destino
-        origin = nodo
-        for a in arista:
-            sValue = min(a)
-            # chequear 
+        if(origin == nodo):
+            for k,value in arista.items():
+                if(destiny == k):
+                    if(value != -1):
+                        path.append(value)
 
+            keyNode = min(arista, key=arista.get)
+            mini = arista[keyNode]
+            camino = camino + mini
+            origin = keyNode
 
-        
+            dijkstra(info,origin,destiny)
+       
 
 if __name__ == "__main__":
     # numero de nodos 
@@ -21,19 +28,26 @@ if __name__ == "__main__":
     # value = lista de aristas en orden 
     info={}
     for i in range(len(nodos)):
-        aristas = []
+        aristas = {}
         for j in range(len(nodos)):
             if(i != j):
                 num = int(input("Ingrese la conexion entre "+ str(nodos[i])+ " - "+ str(nodos[j]) +": " ))
-                aristas.append(num)
+                aristas[nodos[j]] = num
         info[nodos[i]] = aristas
+
     # origen y destino
     origin = int(input("Ingrese el origen: "))
     destiny = int(input("Ingrese a donde se dirige: "))
     
-    origin = 1
-    destiny = 4
-    
-    prueba = {1: [6, 2], 2: [6, -1], 3: [2, -1]}
+    # print(info)
 
-    dijkstra(prueba, origin, destiny)
+    # origin = 1
+    # destiny = 4
+    
+    # arista = {1:6, 2:3, 3:1}
+    # key = min(arista, key=arista.get)
+    # print(key)
+    # print(arista[min(arista, key=arista.get)])
+
+
+    dijkstra(info, origin, destiny)
